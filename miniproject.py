0919ee52a467e7c57a5f4506082c846966d878fe
bmikucka <barbara.mikucka.18@ucl.ac.uk>
2021-10-14 13:58:05 +0100

    just adding files but not printing

--- miniproject.py
+++ miniproject.py
@@ -1,30 +1,30 @@
 import sys
 
-file1 = sys.argv[1]
-file2 = sys.argv[2]
+sprotseq = sys.argv[1]
+pdbseq = sys.argv[2]
 
-print ('first argument was:' + file1)
-print ("second file was:" + file2)
+print(sprotseq)
+print(pdbseq)
 
-def read_file(filename):
-	"""Read input files and return without first line"""
-	with open(filename) as file:
-		return file.readlines().upper() [1:]
+#def read_file(filename):
+	#"""Read input files and return without first line"""
+	#with open(filename) as file:
+		#return file.readlines().upper() [1:]
 
-def no_breaks(file):
-	"""Gets rid of the rows in the file"""
-	return file.strip
-
-
-def compare_seqs (sprotseq, pdbseq):
-	"""Compare 2 sequences to identity whether the PDB sequence pdbseq is the same as the swiss prot sequence sprotseq"""
-	read_file(sprotseq)
-	no_breaks(sprotseq)
-	read_file(pdbseq)
-	no_breaks(pdbseq)
-	print(pdbseq, sprotseq)
-	return True if pdbseq in sprotseq else False
+#def no_breaks(file):
+	#"""Gets rid of the rows in the file"""
+	#return file.strip
 
 
-compare_seqs(sprotseq, pdbseq_nonmut)
+#def compare_seqs (sprotseq, pdbseq):
+	#"""Compare 2 sequences to identity whether the PDB sequence pdbseq is the same as the swiss prot sequence sprotseq"""
+	#read_file(sprotseq)
+	#no_breaks(sprotseq)
+	#read_file(pdbseq)
+	#no_breaks(pdbseq)
+	#print(pdbseq, sprotseq)
+	#return True if pdbseq in sprotseq else False
+
+
+#compare_seqs(sprotseq, pdbseq_nonmut)
 
