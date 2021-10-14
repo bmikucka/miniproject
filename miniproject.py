8ac161546a802ccbb7256b928b320021cea24d67
bmikucka <barbara.mikucka.18@ucl.ac.uk>
2021-10-14 14:18:36 +0100

    added functions

--- miniproject.py
+++ miniproject.py
@@ -3,28 +3,22 @@ import sys
 sprotseq = sys.argv[1]
 pdbseq = sys.argv[2]
 
-print(sprotseq)
-print(pdbseq)
+def read_file(filename):
+	"""Read input files and return as uppercase"""
+	with open(filename) as file:
+		return file.readlines().upper() [1:]
 
-#def read_file(filename):
-	#"""Read input files and return without first line"""
-	#with open(filename) as file:
-		#return file.readlines().upper() [1:]
+def no_breaks(file):
+	"""Gets rid of the rows in the file"""
+	return file.strip
 
-#def no_breaks(file):
-	#"""Gets rid of the rows in the file"""
-	#return file.strip
+def compare_seqs (sprotseq, pdbseq):
+	"""Compare 2 sequences to identity whether the PDB sequence pdbseq is the same as the swiss prot sequence sprotseq"""
+	read_file(sprotseq)
+	no_breaks(sprotseq)
+	read_file(pdbseq)
+	no_breaks(pdbseq)
+	print(pdbseq, sprotseq)
+	return True if pdbseq in sprotseq else False
 
-
-#def compare_seqs (sprotseq, pdbseq):
-	#"""Compare 2 sequences to identity whether the PDB sequence pdbseq is the same as the swiss prot sequence sprotseq"""
-	#read_file(sprotseq)
-	#no_breaks(sprotseq)
-	#read_file(pdbseq)
-	#no_breaks(pdbseq)
-	#print(pdbseq, sprotseq)
-	#return True if pdbseq in sprotseq else False
-
-
-#compare_seqs(sprotseq, pdbseq_nonmut)
 
