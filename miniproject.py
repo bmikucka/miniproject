397dce2ca1c3821d8e5c209875d46759e5bdbd97
bmikucka <barbara.mikucka.18@ucl.ac.uk>
2021-10-19 12:59:18 +0100

    read_file now deleted the first row

--- miniproject.py
+++ miniproject.py
@@ -1,3 +1,4 @@
+#to run: python3 miniproject.py P11413.faa PDB5ukw_nonmutant.faa
 import sys
 
 sprotseq = sys.argv[1]
@@ -7,38 +8,55 @@ pdbseq = sys.argv[2]
 def read_file(filename):
 	"""Read input files"""
 	with open(filename) as file:
-		file = file.read().replace('\n', '') 
-		return file 
+		rows = file.readlines()[1:]
+		combined = ''.join(rows)
+		combined = combined.replace('\n', '') 
+		return combined 
 
-print("Read file function works if testfile:" + (read_file("test.faa")))
+print("does read_file work?" + (read_file("test.faa")))
+print((read_file(pdbseq)))
 
 
 def edit_file(file):
 	"""reads the file, deletes breaks and makes all characters upper case. deletes first 7 characters"""
 	file = read_file(file)
 	file = file.upper()
-	file = file[7:]
 	return file
 
-print("Edit file function works if TESTFILE:" + (edit_file("test.faa")))
+print((edit_file("test.faa")))
 
 
 #reading the two files and changing them to compare
-#still need to remove the identifier bits off the pdb chain to be able to compare them
 def compare_seqs (sprotseq, pdbseq):
 	"""Compare 2 sequences to identity whether the PDB sequence pdbseq is the same as the swiss prot sequence sprotseq"""
 	sprotseq = edit_file(sprotseq)
 	pdbseq = edit_file(pdbseq)
 	#return True if pdbseq in sprotseq else False
-	return (print("The sequences are the same") if pdbseq in sprotseq else print ("The sequences are different"))
+	return print("The sequences are the same") if pdbseq in sprotseq else print ("The sequences are different")
+
+	
+
 
 #test compare_seq function
-print ("compare seqs works if true:")
+print ("test comparison:")
 print((compare_seqs ("test.faa", "test2.faa")))
 
-print(compare_seqs (sprotseq, pdbseq))
+print(compare_seqs(sprotseq, pdbseq))
 
 #print(edit_file(pdbseq))
+
+#compare characters in sequences 
+def differences (seq1, seq2):
+	"""Prints the number of differences across two strings seq1 and seq 2"""
+	count = sum(1 for a, b in zip(seq1, seq2) if a != b)
+	return print(count)
+
+#slide sequence  - put differences into it, print differences in the smallest number of differences
+
+
+
+
+
 
 
 
